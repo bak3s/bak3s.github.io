acf1bab8d135efe0884b56d39d9ff994ae569d76
Daniel Baker <hello@danielbaker.com.au>
2019-10-29 09:30:36 +1100

    fixed service worker js location

--- _site/service-worker.js
+++ _site/service-worker.js
@@ -1,14 +1,14 @@
 importScripts("/assets/js/workbox-v5.0.0.alpha1/workbox-sw.js");
 workbox.setConfig({modulePathPrefix: "/assets/js/workbox-v5.0.0.alpha1"});
 
-self.__precacheManifest = [{"url":"/index.html","revision":"6096567bc3a050f904a918b34e27c87c"},{"url":"/fire/what-is-the-fire-movement/","revision":"c7ec835631443435d44e31358dfd06ee"},{"url":"/fire/catching-fire/","revision":"ed8d8417fdf5173adabcdfa2061c7381"}];
+self.__precacheManifest = [{"url":"/index.html","revision":"c4a3e15dad456645d3a3089d97afada0"},{"url":"/fire/what-is-the-fire-movement/","revision":"c7ec835631443435d44e31358dfd06ee"},{"url":"/fire/catching-fire/","revision":"c48ed483dc20c4b804d73493345117d6"}];
 
 // set names for both precache & runtime cache
 workbox.core.setCacheNameDetails({
-    prefix: 'dans-gems', // use your app's name
-    suffix: 'v1.0',
-    precache: 'precache',
-    runtime: 'runtime-cache'
+  prefix: "catching-fire", // use your app's name
+  suffix: "v1.0",
+  precache: "precache",
+  runtime: "runtime-cache"
 });
 
 // let Service Worker take control of pages ASAP
@@ -19,25 +19,23 @@ workbox.core.clientsClaim();
 workbox.precaching.precacheAndRoute(self.__precacheManifest);
 
 // use `networkFirst` strategy for `*.html`, like all my posts
-workbox.routing.registerRoute(
-    /\.html$/,
-    new workbox.strategies.NetworkFirst()
-);
+workbox.routing.registerRoute(/\.html$/, new workbox.strategies.NetworkFirst());
 
 // use `networkFirst` strategy for css and js
 workbox.routing.registerRoute(
-    /\.(?:js|css)$/,
-    new workbox.strategies.NetworkFirst()
+  /\.(?:js|css)$/,
+  new workbox.strategies.NetworkFirst()
 );
 
 // use `cacheFirst` strategy for images
 workbox.routing.registerRoute(
-    /assets\/(img|icons)/,
-    new workbox.strategies.CacheFirst()
+  /assets\/(img|icons)/,
+  new workbox.strategies.CacheFirst()
 );
 
 // third party files
 workbox.routing.registerRoute(
-    /^https?:\/\/cdn.staticfile.org/,
-	new workbox.strategies.StaleWhileRevalidate()
+  /^https?:\/\/cdn.staticfile.org/,
+  new workbox.strategies.StaleWhileRevalidate()
 );
+
--- service-worker.js
+++ service-worker.js
@@ -1,9 +1,9 @@
 // set names for both precache & runtime cache
 workbox.core.setCacheNameDetails({
-    prefix: 'dans-gems', // use your app's name
-    suffix: 'v1.0',
-    precache: 'precache',
-    runtime: 'runtime-cache'
+  prefix: "catching-fire", // use your app's name
+  suffix: "v1.0",
+  precache: "precache",
+  runtime: "runtime-cache"
 });
 
 // let Service Worker take control of pages ASAP
@@ -14,25 +14,22 @@ workbox.core.clientsClaim();
 workbox.precaching.precacheAndRoute(self.__precacheManifest);
 
 // use `networkFirst` strategy for `*.html`, like all my posts
-workbox.routing.registerRoute(
-    /\.html$/,
-    new workbox.strategies.NetworkFirst()
-);
+workbox.routing.registerRoute(/\.html$/, new workbox.strategies.NetworkFirst());
 
 // use `networkFirst` strategy for css and js
 workbox.routing.registerRoute(
-    /\.(?:js|css)$/,
-    new workbox.strategies.NetworkFirst()
+  /\.(?:js|css)$/,
+  new workbox.strategies.NetworkFirst()
 );
 
 // use `cacheFirst` strategy for images
 workbox.routing.registerRoute(
-    /assets\/(img|icons)/,
-    new workbox.strategies.CacheFirst()
+  /assets\/(img|icons)/,
+  new workbox.strategies.CacheFirst()
 );
 
 // third party files
 workbox.routing.registerRoute(
-    /^https?:\/\/cdn.staticfile.org/,
-	new workbox.strategies.StaleWhileRevalidate()
+  /^https?:\/\/cdn.staticfile.org/,
+  new workbox.strategies.StaleWhileRevalidate()
 );
