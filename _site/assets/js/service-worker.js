ce5eb1816581746efe1d5c4559dce4863039c99d
Daniel Baker <hello@danielbaker.com.au>
2019-10-29 09:51:16 +1100

    service worker updates

--- _site/assets/js/service-worker.js
+++ _site/assets/js/service-worker.js
@@ -1,7 +1,7 @@
-importScripts("/assets/js/workbox-v5.0.0.alpha1/workbox-sw.js");
-workbox.setConfig({modulePathPrefix: "/assets/js/workbox-v5.0.0.alpha1"});
+importScripts("/./assets/js/workbox-v5.0.0.alpha1/workbox-sw.js");
+workbox.setConfig({modulePathPrefix: "/./assets/js/workbox-v5.0.0.alpha1"});
 
-self.__precacheManifest = [{"url":"/index.html","revision":"88506343e7ce62d5161d6b678cbd6924"},{"url":"/fire/what-is-the-fire-movement/","revision":"c7ec835631443435d44e31358dfd06ee"},{"url":"/fire/catching-fire/","revision":"c48ed483dc20c4b804d73493345117d6"}];
+self.__precacheManifest = [{"url":"/index.html","revision":"e2d9904e458295d3e14a1e8e7e88a31a"},{"url":"/fire/what-is-the-fire-movement/","revision":"c7ec835631443435d44e31358dfd06ee"},{"url":"/fire/catching-fire/","revision":"c48ed483dc20c4b804d73493345117d6"}];
 
 // set names for both precache & runtime cache
 workbox.core.setCacheNameDetails({
