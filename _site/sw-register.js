3890ac097a8229948f1829fa90b7bbe7f4b5e7ee
Daniel Baker <hello@danielbaker.com.au>
2019-10-29 16:39:21 +1100

    using new service worker

--- _site/sw-register.js
+++ _site/sw-register.js
@@ -1,5 +1,5 @@
         if ('serviceWorker' in navigator) {
-            navigator.serviceWorker.register('/assets/js/service-worker.js?v=1572327194').then(function(reg) {
+            navigator.serviceWorker.register('/assets/js/service-worker.js?v=1572327546').then(function(reg) {
                 reg.onupdatefound = function() {
                     var installingWorker = reg.installing;
                     installingWorker.onstatechange = function() {
