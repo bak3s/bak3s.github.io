60b4831f09f6a847e1101b870e20e0cea1cc332d
Daniel Baker <hello@danielbaker.com.au>
2019-11-14 10:35:05 +1100

    ssl fix

--- _site/sw-register.js
+++ _site/sw-register.js
@@ -1,5 +1,5 @@
         if ('serviceWorker' in navigator) {
-            navigator.serviceWorker.register('/assets/js/service-worker.js?v=1573624202').then(function(reg) {
+            navigator.serviceWorker.register('/assets/js/service-worker.js?v=1573624360').then(function(reg) {
                 reg.onupdatefound = function() {
                     var installingWorker = reg.installing;
                     installingWorker.onstatechange = function() {
