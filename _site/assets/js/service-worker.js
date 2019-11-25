f85e2de2ccc7cf23c90f2717a4b2e40e2ff0a4af
Daniel Baker <hello@danielbaker.com.au>
2019-11-25 16:21:20 +1100

    updated description seo

--- _site/assets/js/service-worker.js
+++ _site/assets/js/service-worker.js
@@ -1,7 +1,7 @@
                         importScripts("/assets/js/workbox-v3.6.3/workbox-sw.js");
             workbox.setConfig({modulePathPrefix: "/assets/js/workbox-v3.6.3"});
 
-            self.__precacheManifest = [{"url":"/index.html","revision":"7d8618213740da4d53f2ff8802e7257c"},{"url":"/fire/catching-fire-my-journey-to-finanical-independence/","revision":"ed5093fba783d4e5eaf5217943b70b3d"},{"url":"/investing/books/learnings-from-the-barefoot-investor/","revision":"5fe96c4a94d4255c71b44d21dbc53f43"}];
+            self.__precacheManifest = [{"url":"/index.html","revision":"415266daeb34f705915996c98eb46ed0"},{"url":"/fire/catching-fire-my-journey-to-finanical-independence/","revision":"ed5093fba783d4e5eaf5217943b70b3d"},{"url":"/investing/books/learnings-from-the-barefoot-investor/","revision":"5fe96c4a94d4255c71b44d21dbc53f43"}];
             // set names for both precache & runtime cache
 workbox.core.setCacheNameDetails({
   prefix: 'catching-fire',
--- _site/sw-register.js
+++ _site/sw-register.js
@@ -1,5 +1,5 @@
         if ('serviceWorker' in navigator) {
-            navigator.serviceWorker.register('/assets/js/service-worker.js?v=1574657934').then(function(reg) {
+            navigator.serviceWorker.register('/assets/js/service-worker.js?v=1574659260').then(function(reg) {
                 reg.onupdatefound = function() {
                     var installingWorker = reg.installing;
                     installingWorker.onstatechange = function() {
