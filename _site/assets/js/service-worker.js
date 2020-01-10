48f8ea72baef98cb3b32ba8ebe107e58d26c27c7
Daniel Baker <hello@danielbaker.com.au>
2020-01-10 15:19:30 +1100

    check in build

--- _site/assets/js/service-worker.js
+++ _site/assets/js/service-worker.js
@@ -1,7 +1,7 @@
                         importScripts("/assets/js/workbox-v3.6.3/workbox-sw.js");
             workbox.setConfig({modulePathPrefix: "/assets/js/workbox-v3.6.3"});
 
-            self.__precacheManifest = [{"url":"/index.html","revision":"e0507fd84ba264bd205ad6487946cde4"},{"url":"/investing/books/learnings-from-the-barefoot-investor/","revision":"3a53a433c0f983e9bf2e80f8d195eee0"},{"url":"/fire/get-started-with-financial-independence/","revision":"1bb5d81f005c0c441de3533663252d22"}];
+            self.__precacheManifest = [{"url":"/index.html","revision":"4e4ad072122832fd318c5bb2eaa0f5d1"},{"url":"/investing/books/learnings-from-the-barefoot-investor/","revision":"3a53a433c0f983e9bf2e80f8d195eee0"},{"url":"/fire/get-started-with-financial-independence/","revision":"1bb5d81f005c0c441de3533663252d22"}];
             // set names for both precache & runtime cache
 workbox.core.setCacheNameDetails({
   prefix: 'catching-fire',
