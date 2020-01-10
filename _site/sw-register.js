a6baf3030573fb4c31615a740fcef69b7273fb3d
Daniel Baker <hello@danielbaker.com.au>
2020-01-10 17:00:12 +1100

    css fixes

--- _site/sw-register.js
+++ _site/sw-register.js
@@ -1,5 +1,5 @@
         if ('serviceWorker' in navigator) {
-            navigator.serviceWorker.register('/assets/js/service-worker.js?v=1578635793').then(function(reg) {
+            navigator.serviceWorker.register('/assets/js/service-worker.js?v=1578635988').then(function(reg) {
                 reg.onupdatefound = function() {
                     var installingWorker = reg.installing;
                     installingWorker.onstatechange = function() {
